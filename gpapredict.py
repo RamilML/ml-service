99fedffbfd5093fa65b3474722f3180304585005
Ramil <hismatulinramil1@gmail.com>
2026-02-04 15:32:32 +0600

    Update gpapredict

--- gpapredict.py
+++ gpapredict.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
 df = pd.read_csv("Student_performance_data _.csv")
+features = [
+    "Age",
+    "Gender",
+    "StudyTimeWeekly",
+    "Absences",
+    "Extracurricular"
+]
+target = "GPA"
+X = df[features]
+y = df[target]
+
 print(df.head())
 print(df.shape)
-print(print(df.columns))
+print(df.columns)
